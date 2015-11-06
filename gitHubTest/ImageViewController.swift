0d8ea6aa27fbfb77a4cd4fbe84bb464bb2b60570
student1 <student1@jh-mbair-158-11.d214.org>
2015-11-06 09:54:04 -0600

    New: ImageViewController and segways

---
+++ gitHubTest/ImageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImageViewController.swift
+//  gitHubTest
+//
+//  Created by student1 on 11/6/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
